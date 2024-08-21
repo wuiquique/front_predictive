9a73d5f4e19f3c2ac0fdb61e15873c2ccda4e96f
wuiquique <luisenriquem15@gmail.com>
2024-08-20 23:47:19 -0600

    Prediction ui beggining

--- src/pages/Songs/Detail.jsx
+++ src/pages/Songs/Detail.jsx
@@ -3,12 +3,16 @@ import { checkToken } from "../../utils/auth";
 import { useParams } from "react-router-dom";
 import { useEffect } from "react";
 import { useState } from "react";
+import { AiOutlineLoading } from "react-icons/ai";
+import "./Detail.css";
 
 function SongDetail() {
   const { id } = useParams();
 
   const [track, setTrack] = useState({});
   const [features, setFeatures] = useState({});
+  const [prediction, setPrediction] = useState({});
+  const [loading, setLoading] = useState(false);
 
   const formater = new Intl.ListFormat("en");
 
@@ -22,7 +26,7 @@ function SongDetail() {
         },
       })
       .then((res) => {
-        // console.log(res.data);
+        console.log(res.data);
         setTrack(res.data);
       });
   };
@@ -43,6 +47,7 @@ function SongDetail() {
   };
 
   const getPrediction = () => {
+    setLoading(true);
     const data = {
       year: track.album.release_date.split("-")[0],
       duration: features.duration_ms,
@@ -52,12 +57,16 @@ function SongDetail() {
       tempo: features.tempo,
       signature: features.time_signature,
     };
-    axios.post("http://localhost:8000/predict", data).then((res) => {
-      console.log(res.data);
-    })
-    .catch((err) => {
-        console.error(err)
-    })
+    axios
+      .post("http://localhost:8000/predict", data)
+      .then((res) => {
+        console.log(res.data);
+        setPrediction(res.data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
   };
 
   useEffect(() => {
@@ -76,7 +85,9 @@ function SongDetail() {
           className="w-[160px] md:w-[320px] aspect-square object-cover"
         />
         <div className="flex flex-col justify-center ms-4">
-          <h1 className="text-4xl md:text-8xl font-extrabold">{track.name}</h1>
+          <h1 className="text-4xl md:text-8xl font-extrabold max-w-3xl">
+            {track.name}
+          </h1>
           <h2 className="mt-2 text-xl md:text-2xl text-gray-400">
             {formater.format(track?.artists?.map((a) => a?.name))}
           </h2>
@@ -92,10 +103,41 @@ function SongDetail() {
           </a>
         </div>
       </div>
-      <div className="my-20 flex flex-col items-center">
-        <h1 className="text-4xl font-semibold tracking-widest mb-4 text-gray-400">HIT OR MISS?</h1>
-        <button onClick={getPrediction} className="text-2xl bg-[#ff4a01] p-2 w-60 rounded-full">Predict Now</button>
-      </div>
+
+      <div className="bg-zinc-800 w-full h-1 my-10"></div>
+
+      {prediction.prediction ? (
+        <div className="flex justify-center">
+          <div className="bg-zinc-800 p-6 rounded-xl shadow-zinc-700 shadow-sm">
+            <div className="bg-[#ff4a01] w-96 h-48 rounded-t-full relative">
+              <div className="absolute bottom-0 left-1/2 -translate-x-1/2 bg-zinc-800 w-72 h-36 rounded-t-full flex justify-center items-end text-6xl">
+                {(prediction.prediction * 100).toFixed(0)}%
+              </div>
+            </div>
+          </div>
+        </div>
+      ) : (
+        <div className="my-20 flex flex-col items-center">
+          {loading ? (
+            <AiOutlineLoading
+              className="animate-spin fill-[#ff4a01] mt-6"
+              size={36}
+            />
+          ) : (
+            <>
+              <h1 className="text-4xl text-gray-400 font-semibold tracking-wider mb-4 relative border-r-2 border-solid border-[rgba(255,255,255,.75)] text-center whitespace-nowrap overflow-hidden anim-typewriter">
+                HIT OR MISS?
+              </h1>
+              <button
+                onClick={getPrediction}
+                className="text-2xl bg-[#ff4a01] p-2 w-60 rounded-full"
+              >
+                Predict Now
+              </button>
+            </>
+          )}
+        </div>
+      )}
     </div>
   );
 }
