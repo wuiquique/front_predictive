97ff3de2ee809ca781ae39e6c8907a039e7326c7
wuiquique <luisenriquem15@gmail.com>
2024-08-28 22:15:02 -0600

    footer responsive fix

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -54,20 +54,20 @@ function MainLayout() {
       <main>
         <Outlet />
       </main>
-      <footer className="bg-zinc-800 px-20 py-6 grid grid-cols-2">
-        <div className="flex items-center">
+      <footer className="bg-zinc-800 px-20 py-6 grid grid-cols-1 gap-6 sm:gap-0 sm:grid-cols-2">
+        <div className="flex items-center justify-center sm:justify-start">
           <img src="/Home3.png" className="w-20" />
           <div className="ml-4">
             <h5>Luis Enrique Menéndez</h5>
             <h5 className="text-gray-400">lmenendez@unis.edu.gt</h5>
           </div>
         </div>
-        <div className="flex items-center justify-end">
+        <div className="flex items-center justify-center sm:justify-end">
           <a href="https://www.linkedin.com/in/luis-enrique-menendez-figueroa-6953a925b/">
-            <FaLinkedin size={26} />
+            <FaLinkedin size={32} className="text-white hover:text-gray-400" />
           </a>
-          <a href="https://github.com/wuiquique" className="ml-2">
-            <FaGithub size={26} />
+          <a href="https://github.com/wuiquique" className="ml-4 text-white hover:text-gray-400">
+            <FaGithub size={32} />
           </a>
         </div>
       </footer>
