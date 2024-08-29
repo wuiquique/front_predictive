bc8b23e8519f00b56e84e0eeeceb7388a841eb3d
wuiquique <luisenriquem15@gmail.com>
2024-08-28 22:15:25 -0600

    little fix in code format

--- src/components/TopTrack.jsx
+++ src/components/TopTrack.jsx
@@ -2,7 +2,7 @@ import { MdArrowForward } from "react-icons/md";
 import { useNavigate } from "react-router-dom";
 
 function TopTrack({ item }) {
-  function convertDuration(ms) {
+  const convertDuration = (ms) => {
     const minutes = Math.floor(ms / 60000);
     const seconds = ((ms % 60000) / 1000).toFixed(0);
     return `${minutes}:${seconds < 10 ? "0" : ""}${seconds}`;
